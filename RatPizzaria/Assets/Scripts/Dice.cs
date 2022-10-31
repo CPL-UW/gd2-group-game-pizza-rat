4002a7f8abfdc534e2beb40450aed6bf868b3bbc
hhhhhheng <wei73@wisc.edu>
2022-10-31 11:33:03 -0500

    Allow user to decide if they want to take the ingredient or not

--- RatPizzaria/Assets/Scripts/Dice.cs
+++ RatPizzaria/Assets/Scripts/Dice.cs
@@ -7,12 +7,14 @@ public class Dice : MonoBehaviour {
     private SpriteRenderer rend;
     private int whosTurn = 1;
     private bool coroutineAllowed = true;
+    private int maxDiceNum = 0;
 
-	// Use this for initialization
-	private void Start () {
+    // Use this for initialization
+    private void Start () {
         rend = GetComponent<SpriteRenderer>();
         diceSides = Resources.LoadAll<Sprite>("DiceSides/");
-        rend.sprite = diceSides[5];
+        maxDiceNum = GameObject.Find("Player1").GetComponent<Player>().maxDice;
+        rend.sprite = diceSides[maxDiceNum - 1];
 	}
 
     private void OnMouseDown()
@@ -25,22 +27,24 @@ public class Dice : MonoBehaviour {
     {
         coroutineAllowed = false;
         int randomDiceSide = 0;
+
         for (int i = 0; i <= 20; i++)
         {
-            randomDiceSide = Random.Range(0, 6);
+            randomDiceSide = Random.Range(0, this.maxDiceNum);
             rend.sprite = diceSides[randomDiceSide];
             yield return new WaitForSeconds(0.05f);
         }
 
         GameControl.diceSideThrown = randomDiceSide + 1;
-        if (whosTurn == 1)
-        {
-            GameControl.MovePlayer(1);
-        } else if (whosTurn == -1)
-        {
-            GameControl.MovePlayer(2);
-        }
-        whosTurn *= -1;
+        GameControl.MovePlayer(whosTurn);
+        whosTurn++;
+        if (whosTurn == 3) whosTurn = 1;
+
         coroutineAllowed = true;
+    }
+
+    public void RefreshDiceNumber(int maxDiceNum) {
+        this.maxDiceNum = maxDiceNum;
+        rend.sprite = diceSides[maxDiceNum - 1];
     }
 }
