c94cf3adde03fd700a407673983eafedaaadacf3
hhhhhheng <wei73@wisc.edu>
2022-11-09 19:45:11 -0600

    Fix HoverTip

--- RatPizzaria/Assets/Scripts/HoverTip.cs
+++ RatPizzaria/Assets/Scripts/HoverTip.cs
@@ -1,25 +1,29 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 using System.Collections;
+using System;
 
 public class HoverTip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
 
     public string tipToShow;
     private float timeToWait = 0.5f;
+    private Boolean onHover = false;
 
     public void OnPointerEnter(PointerEventData eventData) {
+        onHover = true;
         StopCoroutine(StartTimer());
         StartCoroutine(StartTimer());
     }
 
     public void OnPointerExit(PointerEventData eventData) {
+        onHover = false;
         StopCoroutine(StartTimer());
         HoverTipManager.OnMouseLoseFocus();
     }
 
     private void ShowTip() {
         if (tipToShow == null) return;
-        HoverTipManager.OnMouseHover(tipToShow, Input.mousePosition);
+        if (onHover) HoverTipManager.OnMouseHover(tipToShow, Input.mousePosition);
     }
 
     private IEnumerator StartTimer() {
